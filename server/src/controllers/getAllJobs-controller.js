8256f8775a63143d6a4ce4abfca7a9e0f5f24b9d
SOHEL RANA <sohelrana104322@gmail.com>
2024-07-03 00:28:49 +0600

    working in getAllJobsTesting route

--- server/src/controllers/getAllJobs-controller.js
+++ server/src/controllers/getAllJobs-controller.js
@@ -16,9 +16,24 @@ const getAllJobs = async (req, res) => {
   }
 };
 const getAllJobsTesting = async (req, res) => {
-  // const { salaryType, minPrice, maxPrice } = req.query;
-  // console.log(minPrice, "minPrice", typeof minPrice);
-  const myData = await postJobModel.find(req.query);
+  const { location, salary, exprience, employeement, name } = req.query;
+  const queryObj = {};
+  
+  if(location){
+    queryObj.jobLocation = location;
+  }
+
+  if(salary){
+    queryObj.salaryType = salary;
+    console.log(queryObj.salaryType, "queryObj.salaryType");
+  }
+
+  if(name){
+    queryObj.companyName = name;
+  }
+
+  console.log(queryObj);
+  const myData = await postJobModel.find(queryObj);
 
   res.status(200).json({
     status: "success",
